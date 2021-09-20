7c47034a6604c380d6246ed0b33bbb63dfa2782d
Eugen Bogdanovich <eugen@oyenetwork.com>
2021-09-20 14:58:36 +0100

    add csrf token

--- resources/views/completion.blade.php
+++ resources/views/completion.blade.php
@@ -3,6 +3,6 @@
 
     <textarea rows="5" cols="50" name="text"></textarea>
     <button type="submit">Complete</button>
-
+    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
     <pre>{{$text}}</pre>
 </form>
