c421143190a862e929011a367e045cf8fa96e4bd
Eugen Bogdanovich <eugen@oyenetwork.com>
2021-10-02 16:12:23 +0100

    change welcome email text

--- resources/views/emails/welcome-email.blade.php
+++ resources/views/emails/welcome-email.blade.php
@@ -1,4 +1,6 @@
-<h2>Welcome, we're much pleased to have you as a user</h2>
+<h2>Welcome, {{$name}}</h2>
+
+<p>we're much pleased to have you as a user</p>
 
 <p>Atm we are in beta testing, if you would like to test our service, you can do so here</p>
 <strong><a href="https://beta.justsayit.works/">beta.justsayit.works</a></strong>
